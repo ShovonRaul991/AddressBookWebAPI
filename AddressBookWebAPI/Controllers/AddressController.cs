aafa72ead21f7a3d18bd9a67571bb73720d3e19e
Shovon Raul <shovon.r@keka.com>
2023-02-27 15:23:52 +0530

    Add project files.

---
+++ AddressBookWebAPI/Controllers/AddressController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AddressBookWebAPI.Data;
+using AddressBookWebAPI.Model;
+
+namespace AddressBookWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddressController : Controller
+    {
+        private readonly AddressBookAPIDbContext _context;
+
+        public AddressController(AddressBookAPIDbContext db)
+        {
+            _context= db;
+        }
+
+        [HttpGet]
+        [Route("GetAllAddress")]
+        public IActionResult Index()
+        {
+            return Ok(_context.Addresses.ToList());
+
+        }
+
+        [HttpGet]
+        [Route("GetAddress/{id}")]
+        public IActionResult GetAddressDetails(int id)
+        {
+            var tempAddress = _context.Addresses.Find(id);
+            if (tempAddress == null)
+            {
+                return NotFound();
+            }
+            return Ok(tempAddress);
+        }
+
+        [HttpPost]
+        [Route("AddAddress")]
+        public IActionResult GetAddress(AddAddress obj)
+        {
+            var tempAddress = new Address()
+            {
+                Name = obj.Name,
+                email = obj.email,
+                phone = obj.phone,
+                landline = obj.landline,
+                website = obj.website,
+                AddressDetails = obj.AddressDetails,
+            };
+            _context.Addresses.Add(tempAddress);
+            _context.SaveChanges();
+            return Ok(tempAddress);
+        }
+
+        [HttpPut]
+        [Route("UpdateAddress/{id}")]
+        public IActionResult UpdateAddressDetails(int id, AddAddress obj)
+        {
+            var tempAddress = _context.Addresses.Find(id);
+            if(tempAddress== null)
+            {
+                return NotFound();
+            }
+            tempAddress.Name = obj.Name;
+            tempAddress.email = obj.email;
+            tempAddress.phone = obj.phone;
+            tempAddress.landline = obj.landline;
+            tempAddress.website = obj.website;
+            tempAddress.AddressDetails = obj.AddressDetails;
+
+            return Ok(tempAddress);
+        }
+
+        [HttpDelete]
+        [Route("DeleteAddress/{id}")]
+        public IActionResult DeleteAddress(int id)
+        {
+            var tempAddress = _context.Addresses.Find(id);
+            if(tempAddress== null)
+            {
+                return BadRequest();
+            }
+            _context.Remove(tempAddress);
+            _context.SaveChanges();
+            return Ok(tempAddress);
+
+        }
+    }
+}
---
+++ AddressBookWebAPI/Model/Address.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBookWebAPI.Model
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string email { get; set; }
+        public long phone { get; set; }
+        public long landline { get; set; }
+        public string website { get; set; }
+        public string AddressDetails { get; set; }
+
+    }
+}
